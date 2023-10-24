c46d2b650dfa4a1c43738674f0b913cffee2eb45
Ufuk <ufukkirmizigedik1984@gmail.com>
2023-10-24 22:51:42 +0300

    feat : contact updated

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index,contact
+from .views import index,contact,blog
 
 
 urlpatterns = [
     path('',index,name='index'),
-    path('contact',contact,name='contact')
-]
+    path('contact',contact,name='contact'),
+    path('blog',blog,name='blog')]
--- core/views.py
+++ core/views.py
@@ -6,4 +6,8 @@ def index(request):
     return render(request,'index.html')
 
 def contact(request):
-    return render(request,'Contact.html')
+    return render(request,'contact.html')
+
+
+def blog(request):
+    return render(request,'blog.html')

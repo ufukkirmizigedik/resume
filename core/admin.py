27a83db7720209fa978e89fecd9190fbcd29a708
Ufuk <ufukkirmizigedik1984@gmail.com>
2023-11-16 10:17:14 +0300

    feat : add general setting model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from core.models import GeneralSettings
+from core.models import *
 from django.contrib.auth.models import User
 
 User.objects.filter(is_superuser=True)
 
 # Register your models here.
-admin.site.register(GeneralSettings)
+@admin.register(GeneralSettings)
 
 
 class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id','name','description','parameters','update_date','create_date']
+    search_fields = ['name','description']
+    list_editable = ['name','description']
     class Meta:
         model = GeneralSettings
